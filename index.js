dca338074bfd701c4d839a4b9220623ddfe2f471
Stephen Moore <judo2000@comcast.net>
2022-02-21 17:29:54 -0700

    Initial commit create apollo server and connect to mongodb

---
+++ index.js
@@ -0,0 +1,57 @@
+require('dotenv');
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const Post = require('./models/Post');
+const User = require('./models/User');
+const { MONGODB } = require('./config');
+
+const typeDefs = gql`
+    type Post{
+        id: ID!,
+        body: String!,
+        createdAt: String!,
+        username: String!
+    }
+    type Query{
+        getPosts: [Post]
+    }
+`;
+
+const resolvers = {
+    Query: {
+        async getPosts(){
+            try {
+                const posts = await Post.find();
+                return posts;
+            } catch (err) {
+                throw new Error(err)
+            }
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+// connect to mlab database
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log('MongoDB Connected');
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  })
+  .catch(err => {
+    console.error(err)
+  })
+
+// server.listen({ port: 5000})
+//     .then(res => {
+//         console.log(`Server running at ${res.url}`)
+//     })
