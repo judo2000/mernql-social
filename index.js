6b84fad11f1fe7f403d401276a4890a091d2b7e9
Stephen Moore <judo2000@comcast.net>
2022-02-23 06:55:08 -0700

    Add validations to user login

--- index.js
+++ index.js
@@ -1,32 +1,21 @@
-require('dotenv');
-const { ApolloServer } = require('apollo-server');
-const mongoose = require('mongoose');
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
 
-const typeDefs = require('./graphql/typeDefs');
-const resolvers = require('./graphql/resolvers');
-const { MONGODB } = require('./config');
-
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config.js");
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+  typeDefs,
+  resolvers,
 });
 
-// connect to mlab database
 mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
-    console.log('MongoDB Connected');
+    console.log("MongoDB Connected");
     return server.listen({ port: 5000 });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
-  })
-  .catch(err => {
-    console.error(err)
-  })
-
-// server.listen({ port: 5000})
-//     .then(res => {
-//         console.log(`Server running at ${res.url}`)
-//     })
+  });
